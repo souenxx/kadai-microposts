f8fd363d7f506898787b9be2d0290f84ad37733e
souen <souen_xx@yahoo.co.jp>
2018-05-12 12:49:12 +0000

    re

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to:'users#new'
+  get 'signup', to: 'users#new'
   resources :users, only:[:index, :show, :new, :create] do
     member do
       get :followings
